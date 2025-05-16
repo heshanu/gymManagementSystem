ee25675e4c6f3abccd1f719863348a811f1770aa
heshanu <heshanu97@gmail.com>
2025-05-17 01:52:58 +0530

    implemented eureka service

---
+++ servericeRegister/src/test/java/com/example/servericeRegister/ServericeRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servericeRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServericeRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
